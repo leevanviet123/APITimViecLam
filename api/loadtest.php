4a791824121c2473ebe0ce152b12c51e3c6619e0
1080yoh <hakemena@gmail.com>
2019-07-28 21:19:44 +0700

    đổi chỗ hiển thị tên về bảng customer

---
+++ api/loadtest.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once './connection.php';
+if ($isOK) {	
+    $sql = "SELECT * from test";    
+    $result = mysqli_query($connect, $sql);
+
+    // Đọc về dữ liệu    
+    // Trả về JSON
+    $rows = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+        $rows[] = $row;
+    }
+    echo json_encode($rows);
+}
+
+?>
--- api/register.php
+++ api/register.php
@@ -3,10 +3,9 @@ require_once './connection.php';
 if($isOK){
 	$newuser = $_REQUEST['username'];
 	$newpass = $_REQUEST['password'];
-	$newname = $_REQUEST['name_display'];
 	$newemail = $_REQUEST['email'];
 
-	$sql = "INSERT INTO `account` (`username`, `password`, `name_displayed`, `email_restore`) VALUES ('{$newuser}', '{$newpass}', N'{$newname}', '{$newemail}')";
+	$sql = "INSERT INTO `account` (`username`, `password`, `email_restore`) VALUES ('{$newuser}', '{$newpass}', '{$newemail}')";
 	$result = mysqli_query($connect, $sql);
 	if($result){
 		echo json_encode($mess_susccess);
